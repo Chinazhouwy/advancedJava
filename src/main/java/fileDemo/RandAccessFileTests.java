65c98013413e1d1201cbab334a06e8ce2ec91afb
zhouwy <chinazhouwy@gmail.com>
2018-06-18 20:01:32 +0800

    IO流的学习

---
+++ src/main/java/fileDemo/RandAccessFileTests.java
@@ -0,0 +1,42 @@
+package fileDemo;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+public class RandAccessFileTests {
+
+    @Test
+    public void writeFixStr() throws FileNotFoundException {
+        RandomAccessFile raf = new RandomAccessFile("aa.dat","rw");
+
+    }
+
+    @Test
+    public void readFixStr(){
+        System.out.println(Charset.forName("UTF-8").aliases());
+//        System.out.println(Charset.forName("uft-8").aliases());
+    }
+
+    @Test
+    public void charset(){
+        String s = "abcdef";
+        Charset cset = Charset.forName("UTF-8");
+        ByteBuffer buffer = cset.encode(s);
+        byte[] a = buffer.array();
+        for(byte b : a){
+            System.out.println(b);
+        }
+
+        System.out.println("=========");
+
+        ByteBuffer bbuf = ByteBuffer.wrap(a);
+        System.out.println(cset.decode(bbuf).toString());
+
+    }
+
+}
